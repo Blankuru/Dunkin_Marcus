ae23933aa36314da0884fbfcd625535d49c08357
Blankuru <19227@my.sanctamaria.school.nz>
2022-03-18 15:11:58 +1300

    Created components

---
+++ Components/pickup_v1.py
@@ -0,0 +1,12 @@
+#Bug - accepts blank input
+
+print ("Please enter your pickup information")
+
+#Customer name
+name = input("Please enter your name: ")
+
+#Customer phone
+phone = input("Please enter your phone number: ")
+
+print (name)
+print (phone)
---
+++ Components/pizza_menu_v2.py
@@ -0,0 +1,17 @@
+pizza_names = ['Margherita', 'Pepperoni','Hawaiian','Cheese','Italian','Veggie','Vegan','Chicken Deluxe',
+                'Mega Meat Lovers','Seafood Deluxe','Apricot Chicken Deluxe','BBQ Chicken Deluxe']
+
+pizza_prices = [8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 13.50, 13.50, 13.50, 13.50, 13.50]
+
+
+
+print("How many pizzas would you like to order? ")
+num_pizza = int(input())
+
+def menu():
+    number_pizzas = 12
+
+    for count in range (number_pizzas):
+        print ("{} {} ${:.2f}" .format(count+1, pizza_names[count], pizza_prices[count]))
+
+menu()
