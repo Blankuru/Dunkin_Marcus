af06f1c556fa12a5346dac77669deb90bf332e3d
Blankuru <99222846+Blankuru@users.noreply.github.com>
2022-04-06 18:31:36 +1200

    Fixed Component 10 and improved main function

--- Components/(10)val_int_v2.py
+++ Components/(10)val_int_v2.py
@@ -1,20 +1,20 @@
 from tkinter.messagebox import QUESTION
 
-def order_type(low, high, question): 
- while True:
-    try: 
-        num = int(input())
-        if num >= low and num <= high:
-            return num
-        else:
+def order_type(low, high, question):
+    while True:
+        try:
+            num = int(input(question))
+            if num >= low and num <= high:
+                return num
+            else:
+                print(f"Please enter a number between {low} and {high}")
+        except ValueError:
+            print ("That is not a valid number")
             print(f"Please enter a number between {low} and {high}")
-    except ValueError:
-        print("That is not a valid number")
-        print(f"Please enter a number between {low} and {high}")
 
 LOW = 1
 HIGH = 12
-question = (f"Enter a number between {LOW} and {HIGH} ")
+question = (f"Enter a number between {LOW} and {HIGH}: ")
 
 answer = order_type(LOW, HIGH, question)
 
