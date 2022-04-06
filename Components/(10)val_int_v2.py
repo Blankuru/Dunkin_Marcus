e310185d3055fe16b7c095bb84bdd8aa4c9e4919
Blankuru <19227@my.sanctamaria.school.nz>
2022-04-06 15:13:35 +1200

    fixed component 10 v2

--- Components/(10)val_int_v2.py
+++ Components/(10)val_int_v2.py
@@ -3,19 +3,19 @@ from tkinter.messagebox import QUESTION
 def order_type(low, high, question): 
  while True:
     try: 
-        num = int(input)
+        num = int(input())
         if num >= low and num <= high:
             return num
         else:
-            print(f"Please enter a number between {low}, "and", {high}")
+            print(f"Please enter a number between {low} and {high}")
     except ValueError:
         print("That is not a valid number")
-        print(f"Please enter a number between {low}, "and", {high}")
+        print(f"Please enter a number between {low} and {high}")
 
 LOW = 1
 HIGH = 12
-QUESTION(f"Enter a number between {LOW}, "and", {HIGH}")
+question = (f"Enter a number between {LOW} and {HIGH} ")
 
-answer = order_type(LOW, HIGH, QUESTION)
+answer = order_type(LOW, HIGH, question)
 
 print(answer)
